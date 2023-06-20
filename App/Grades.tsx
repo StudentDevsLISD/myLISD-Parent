fdec648bc5416d6e932ffd3182dfe3c35ab3e0f3
adithc <adith.chandraiah@gmail.com>
2023-06-20 08:34:10 -0500

    idk adith told me to push

--- App/Grades.tsx
+++ App/Grades.tsx
@@ -1,11 +1,22 @@
 import { useNavigation } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { ScrollView, View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+
+interface GradeType {
+  color: string;
+  letter: string;
+}
+
+const getGrade = (score: number): GradeType => {
+  if (score >= 90) return { color: '#00DE64', letter: 'A' };
+  if (score >= 80) return { color: '#3199FE', letter: 'B' };
+  if (score >= 70) return { color: '#F99816', letter: 'C' };
+  return { color: '#FB5B5B', letter: 'D' };
+};
 
 const Grades = () => {
   const [grades, setGrades] = useState<Record<string, string>>({});
-
   const [currentDate, setCurrentDate] = useState('');
 
   useEffect(() => {
@@ -21,7 +32,7 @@ const Grades = () => {
   useEffect(() => {
     const dummyData = {
       Math: "85",
-      English: "92",
+      English: "55",
       Science: "78",
       History: "88",
       Geography: "90",
@@ -32,7 +43,9 @@ const Grades = () => {
 
     setGrades(dummyData);
   }, []);
+  
   const navigation = useNavigation();
+  
   React.useLayoutEffect(() => {
     navigation.setOptions({
       headerLeft: () => (
@@ -45,22 +58,31 @@ const Grades = () => {
       ),
     });
   }, [navigation]);
+  
   return (
     <View>
       <View style={styles.header}>
         <Text style={styles.dateText}>{currentDate}</Text>
         <Text style={styles.headerText}>Grades</Text>
       </View>
-      {Object.entries(grades).map(([subject, grade], index) => (
-        <TouchableOpacity style={styles.gradeContainer} key={index}>
-          <View style={styles.gradeItem}>
-            <Text style={styles.gradeText}>{subject}</Text>
-            <View style={styles.gradeBadge}>
-              <Text style={styles.gradeBadgeText}>{grade + "%"}</Text>
-            </View>
-          </View>
-        </TouchableOpacity>
-      ))}
+      <ScrollView>
+        {Object.entries(grades).map(([subject, grade], index) => {
+          const { color, letter } = getGrade(Number(grade));
+          return (
+            <TouchableOpacity style={styles.gradeContainer} key={index}>
+              <View style={styles.gradeItem}>
+                <Text style={styles.gradeText}>{subject}</Text>
+                <View style={styles.gradeBadge}>
+                  <Text style={styles.gradeBadgeText}>{letter}</Text>
+                </View>
+                <View style={[styles.gradeBadgeColor, { backgroundColor: color }]}>
+                  <Text style={styles.gradeBadgeText}>{grade + "%"}</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 };
@@ -69,7 +91,7 @@ const styles = StyleSheet.create({
   gradeContainer: {
     backgroundColor: '#E6E6E6',
     borderRadius: 10,
-    padding: 15,
+    padding: 12.5,
     marginVertical: 5,
     marginHorizontal: 10,
   },
@@ -83,12 +105,18 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   gradeBadge: {
-    backgroundColor: '#CCCCCC',
+    borderRadius: 8,
+    paddingHorizontal: 11,
+    paddingVertical: 5,
+    marginLeft: 10,
+  },
+  gradeBadgeColor: {
     borderRadius: 8,
     paddingHorizontal: 11,
     paddingVertical: 5,
   },
   gradeBadgeText: {
+
     color: 'black',
     fontSize: 20,
     fontWeight: 'bold',
@@ -102,19 +130,17 @@ const styles = StyleSheet.create({
   headerText: {
     fontSize: 40,
     marginLeft: -95,
-    marginBottom:10,
+    marginBottom: 10,
     marginTop: 10,
     color: "#005987",
     fontWeight: "600",
-    
   },
   dateText: {
     fontSize: 14,
     color: 'gray',
-    marginTop:58,
+    marginTop: 58,
     marginLeft: 14,
   },
 });
-
 
 export default Grades;
