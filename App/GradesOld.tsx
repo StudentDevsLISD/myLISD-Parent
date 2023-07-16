868484ce65ae26a2ce0000061173b28982663e18
CodingPro445 <slowbird1108@gmail.com>
2023-07-16 11:58:55 -0500

    added error logging in when username or password i s wrong

--- App/GradesOld.tsx
+++ App/GradesOld.tsx
@@ -57,6 +57,14 @@ const Grades = () => {
         `http://localhost:8000/?username=${username}&password=${password}`
       );
       const currentClasses = response.data.currentClasses;
+
+      if (currentClasses.length === 0) {
+        Alert.alert('Error', 'Error logging in. Please try again.');
+        setIsLoggedIn(false);
+        setIsLoading(false);
+        return;
+      }
+
       setIsLoading(true);
       console.log(isLoading);
 
@@ -90,6 +98,8 @@ const Grades = () => {
       setGrades(grades);
     } catch (error) {
       console.error('Error fetching grades:', error);
+      Alert.alert('Error', 'An error occurred while fetching grades.');
+      setIsLoggedIn(false);
     }
     setIsLoading(false);
     console.log(isLoading);
